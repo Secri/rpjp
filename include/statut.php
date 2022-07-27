239a564fba8d718156bcc9f4cd0230f5497a6597
Secri <44509048+Secri@users.noreply.github.com>
2022-07-27 15:42:35 +0200

    Add files via upload

--- include/statut.php
+++ include/statut.php
@@ -1,7 +1,6 @@
 <!-- Affiche une liste déroulante qui indique le statut de la publicité -->
 <p class="meta-options field">
     <select id="statut" name="statut" disabled>
-		<option value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'statut', true ) ); ?>"><?php echo esc_attr( get_post_meta( get_the_ID(), 'statut', true ) ); ?></option>
 		<option value="Programmée" <?php if ( handleStatus($post) == 'Programmée' ) { echo "selected";} ?>>Programmée</option>
 		<option value="Brouillon" <?php if ( handleStatus($post) == 'Brouillon' ) { echo "selected";} ?>>Brouillon</option>
 		<option value="Publiée" <?php if ( handleStatus($post) == 'Publiée' ) { echo "selected";} ?>>Publiée</option>
