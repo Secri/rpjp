d64a22bb8900bf8759e30d959a48072cf688ac6f
Secri <44509048+Secri@users.noreply.github.com>
2022-08-01 00:36:46 +0200

    Add files via upload

--- include/ref.php
+++ include/ref.php
@@ -1,8 +1,10 @@
 <p class="meta-options field">
-        <input id="ref"
-            type="text"
-            name="ref"
-			disabled
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'ref', true ) ); ?>">
+    <input 
+		  disabled
+	      id="ref"
+          type="text"
+          name="ref"
+		  value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'ref', true ) ); ?>"
+	>
 </p>
 
