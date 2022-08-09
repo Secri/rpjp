b8b2843a0e11776752bc9edc672b5704aa513a05
Secri <44509048+Secri@users.noreply.github.com>
2022-08-09 19:56:03 +0200

    Update function.php

--- include/function.php
+++ include/function.php
@@ -116,6 +116,18 @@ function RPJP_metabox_callback( $post ) { //paramètres
 
 function RPJP_statut_callback($post){ //statut
 	include plugin_dir_path( __FILE__ ) . './statut.php';
+	?>
+	<!-- Affiche une liste déroulante qui indique le statut de la publicité -->
+	<p class="meta-options field">
+    		<select id="statut" name="statut" disabled>
+			<option value="Programmée" <?php if ( handleStatus($post) == 'Programmée' ) { echo "selected";} ?>>Programmée</option>
+			<option value="Brouillon" <?php if ( handleStatus($post) == 'Brouillon' ) { echo "selected";} ?>>Brouillon</option>
+			<option value="Publiée" <?php if ( handleStatus($post) == 'Publiée' ) { echo "selected";} ?>>Publiée</option>
+			<option value="Dépassée" <?php if ( handleStatus($post) == 'Dépassée' ) { echo "selected";} ?>>Dépassée</option>
+			<option value="Erreur" <?php if ( handleStatus($post) == 'Erreur' ) { echo "selected";} ?> >Erreur</option>
+		</select>
+	</p>
+	<?php
 }
 
 function RPJP_ref_callback($post){ //référence
