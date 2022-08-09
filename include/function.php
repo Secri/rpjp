9a3a7f6d6f7216c22c115dbde23dcacde5a55960
Secri <44509048+Secri@users.noreply.github.com>
2022-08-09 15:14:06 +0200

    Update function.php

--- include/function.php
+++ include/function.php
@@ -119,7 +119,17 @@ function RPJP_statut_callback($post){ //statut
 }
 
 function RPJP_ref_callback($post){ //référence
-	include plugin_dir_path( __FILE__ ) . './ref.php';
+	?>
+	<p class="meta-options field">
+		<input 
+			  disabled
+			  id="ref"
+			  type="text"
+			  name="ref"
+			  value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'ref', true ) ); ?>"
+		>
+	</p>
+	<?php
 }
 
 /*Fonction permettant de sauvegarder le contenu des metabox*/
