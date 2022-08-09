b8045a726d4fd358cba0de3b75c4dcfd008942fc
Secri <44509048+Secri@users.noreply.github.com>
2022-08-09 20:10:18 +0200

    Update function.php

--- include/function.php
+++ include/function.php
@@ -132,13 +132,8 @@ function RPJP_statut_callback($post){ //statut
 function RPJP_ref_callback($post){ //référence
 	?>
 	<p class="meta-options field">
-		<input 
-			  disabled
-			  id="ref"
-			  type="text"
-			  name="ref"
-			  value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'ref', true ) ); ?>"
-		>
+		<input disabled id="ref" type="text" name="ref"
+		       value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'ref', true ) ); ?>" >
 	</p>
 	<?php
 }
