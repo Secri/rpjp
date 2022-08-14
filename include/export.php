be56eaaf33385b5a79b963d72e6eab925927e4d0
Secri <44509048+Secri@users.noreply.github.com>
2022-08-14 21:22:50 +0200

    Add files via upload

--- include/export.php
+++ include/export.php
@@ -20,10 +20,10 @@ function RPJP_add_export_button( $which ) {
 				
 				<input type="date" id="dateDeb" name="debut"></input>
 				<input type="date" id="dateFin" name="fin"></input>
-				
 				<input type="submit" name="export_post_date" id="exporter" class="button button-primary" value="<?php _e('Exporter'); ?>" />
+				<input type="submit" name="export_all_posts" class="button button-primary" value="<?php _e('Exporter toutes les publicités'); ?>" />
+			
 			</form>
-			<input type="submit" name="export_all_posts" class="button button-primary" value="<?php _e('Exporter toutes les publicités'); ?>" />
 		</div>
         <?php
     }
@@ -42,12 +42,26 @@ function RPJP_export_posts() {
 				'post_type' => 'regie_publicitaire',
 				'post_status' => array('publish','draft'),
 				'posts_per_page' => -1,
+				'meta_query' => array(
+										'relation' => 'AND',
+										array(
+											'key'     => 'dateDeb',
+											'value'   => date('Y-m-d', $debut), //Attention au format de la date pour la comparaison !!
+											'type'    => 'DATE',
+											'compare' => '>='
+										),
+										array(
+											'key'     => 'dateFin',
+											'value'   => date('Y-m-d', $fin), //Attention au format de la date pour la comparaison !!
+											'type'    => 'DATE',
+											'compare' => '<='
+										),
+									)
 			);
-		  
-			global $post;
-			$arr_post = get_posts($arg); //on récupère tous les posts qui correspondent aux paramètres
-			if ($arr_post) { //s'il y en a
-		  
+			
+			$my_query = new WP_Query( $arg );
+			if( $my_query->have_posts() ) {
+				
 				//requêtes pour créer le fichier csv et gérer les dates 
 				header('Content-Encoding: UTF-8');
 				header('Content-type: text/csv; charset=UTF-8');
@@ -59,40 +73,46 @@ function RPJP_export_posts() {
 				echo "\xEF\xBB\xBF";
 				
 				fputcsv($file, array('Titre', 'Date de début', 'Date de fin', 'Référence', 'Statut')); //ajoute une ligne indiquant à quoi correspondent les valeurs
-	  
-				//pour chaque post, récupère et affiche les données
-				foreach ($arr_post as $post) {
-					
-					if ( strtotime($post->dateDeb) >= $debut && strtotime($post->dateFin) <= $fin ) {
-						
-						fputcsv($file, array ( 
-									$post->post_title,
-									date('d-m-Y', strtotime($post->dateDeb)),
-									date('d-m-Y', strtotime($post->dateFin)),
-									$post->ref,
-									handleStatus($post)
-								      )
-								);
-					}
-				}
+				
+				
+				while( $my_query->have_posts() ) : $my_query->the_post();
+					fputcsv($file, array ( 
+											get_the_title(get_the_ID()),
+											date('d-m-Y', strtotime( get_post_meta( get_the_ID(), 'dateDeb', true ) )),
+											date('d-m-Y', strtotime( get_post_meta( get_the_ID(), 'dateFin', true ) )),
+											get_post_meta(get_the_ID(), 'ref', true),
+											handleStatus(get_post( get_the_ID(), 'OBJECT' ))
+										 )
+							);
+				endwhile;
 				exit();
+			} else {
+				
+				?>
+					<div class="notice notice-warning is-dismissible" >
+						<p><?php _e( 'Aucune donnée à exporter '. date('d-m-Y', $debut) .' et le '. date('d-m-Y', $fin) .'.', 'RPJP' ); ?></p>
+					</div>
+				<?php
+
 			}
-		}else if($debut > $fin){
+			wp_reset_postdata();
+			
+		} else if( $debut > $fin ) {
 			?>
-			<div class="notice error my-acf-notice is-dismissible" >
-				<p><?php _e( 'La date de début ne peut pas être postérieure à la date de fin.', 'RPJP' ); ?></p>
-			</div>
+				<div class="notice notice-warning is-dismissible" >
+					<p><?php _e( 'La date de début ne peut pas être postérieure à la date de fin.', 'RPJP' ); ?></p>
+				</div>
 			<?php
 		}else{
 			?>
-			<div class="notice error my-acf-notice is-dismissible" >
-				<p><?php _e( 'Veuillez entrer des dates.', 'RPJP' ); ?></p>
-			</div>
+				<div class="notice notice-warning is-dismissible" >
+					<p><?php _e( 'Veuillez entrer des dates.', 'RPJP' ); ?></p>
+				</div>
 			<?php
 		}
     }
-    if(isset($_GET['export_all_posts'])) { //si le bouton "exporter toutes les publicités" est cliqué
-		//on prépare des paramètres pour sélectionner le type de posts voulu
+    if( isset( $_GET['export_all_posts'] ) ) { //si le bouton "exporter toutes les publicités" est cliqué
+		//paramètres pour récupérer les posts regie_publicitaire
         $arg = array(
             'post_type' => 'regie_publicitaire',
             'post_status' => array('publish','draft'),
@@ -101,7 +121,7 @@ function RPJP_export_posts() {
   
         global $post;
         $arr_post = get_posts($arg); //on récupère tous les posts qui correspondent aux paramètres
-        if ($arr_post) { //s'il y en a
+        if (count($arr_post) > 0) { //s'il y en a
   
 			//requêtes pour créer le fichier csv
 			header('Content-Encoding: UTF-8');
@@ -116,19 +136,25 @@ function RPJP_export_posts() {
             
 			fputcsv($file, array('Titre', 'Date de début', 'Date de fin', 'Référence', 'Statut')); //ajoute une ligne indiquant à quoi correspondent les valeurs
 			
-			//pour chaque post, récupère et affiche les données
+			
             foreach ($arr_post as $post) {
 				
 				fputcsv($file, array ( 
-							$post->post_title,
-							date('d-m-Y', strtotime($post->dateDeb)),
-							date('d-m-Y', strtotime($post->dateFin)),
-							$post->ref,
-							handleStatus($post)
-						     )
+										$post->post_title,
+										date('d-m-Y', strtotime($post->dateDeb)),
+										date('d-m-Y', strtotime($post->dateFin)),
+										$post->ref,
+										handleStatus($post)
+						            )
 						);
 			}
             exit();
-        }
+        } else {
+			?>
+				<div class="notice notice-warning is-dismissible" >
+					<p><?php _e( 'Aucune donnée à exporter.', 'RPJP' ); ?></p>
+				</div>
+			<?php
+		}
     }
 }
