a158f96852e47d273efb6acdd5207d37f4e83a71
Secri <44509048+Secri@users.noreply.github.com>
2022-08-16 16:25:45 +0200

    Add files via upload

---
+++ include/js/regen.js
@@ -0,0 +1,20 @@
+var regenBtn = document.getElementById('rpjp_regen_btn');
+
+regenBtn.addEventListener('click', rpjp_validate_regen, false);
+
+function rpjp_validate_regen(e) {
+	e.preventDefault();
+	Swal.fire({
+		icon: 'warning',
+		title: 'Attention',
+		html: 'Voulez-vous vraiment regénérer toutes les références ?<br>Cette action sera <b>IRR&Eacute;VERSIBLE</b>.',
+		showCancelButton: true,
+		cancelButtonText: 'Annuler',
+		cancelButtonColor: '#d33',
+		allowOutsideClick: false
+	}).then((result) => {
+		if (result.isConfirmed) {
+			regenBtn.parentElement.submit();
+		}
+	})
+}
