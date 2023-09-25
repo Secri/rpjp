e46dd6297f4b134afc9cb1b884b71bb9e6d97d37
Secri <44509048+Secri@users.noreply.github.com>
2023-09-25 11:49:02 +0200

    Update unpublish_cron.php

--- include/unpublish_cron.php
+++ include/unpublish_cron.php
@@ -23,6 +23,47 @@
 									'post_status' => 'draft',
 								);
 					wp_update_post( $arg ); //on passe le post en brouillon
+					
+					//Gestion du renouvellement automatique
+					$custom = get_post_custom($value->ID); //On récupère les paramètres custom dans un tableau
+					if(isset( $custom["renew"][0] ) && $custom["renew"][0] == 'on' ) { //Si le post qui vient d'être dépublié est en mode renouvellement auto
+						//calculer la durée de la pub initiale
+						$duration = strtotime ( $value->dateFin ) - strtotime ( $value->dateDeb );
+						//Tableau des données du post à créer 
+						$postarr = array(
+											'post_title'  => $value->post_title,
+											'post_status' => 'draft',            //en draft (pour pouvoir récupérer l'ID et générer la ref
+											'post_author'  => 1,
+											'post_type'   => 'regie_publicitaire',
+											'meta_input'  => array(
+																	'ref'           => '',
+																	'image_desktop' => $value->image_desktop,
+																	'image_mobile'  => $value->image_mobile,
+																	'cpt'           => $value->cpt,
+																	'categ'         => $value->categ,
+																	'follow'        => $value->follow,
+																	'mobile'        => $value->mobile,
+																	'renew'         => $value->renew,
+																	'lien'          => $value->lien,
+																	'dateDeb'       => date( 'Y-m-d', time() ),
+																	'dateFin'       => date( 'Y-m-d', time() + $duration )
+																   ),
+										);
+						$newPostId = wp_insert_post($postarr, true); //On lance l'insertion qui retourne l'ID du post ainsi créé
+						if ( $newPostId && !is_wp_error( $newPostId ) ) { //Si l'opération s'est bien déroulée
+							//Génération de la ref
+							$options = get_option('RPJP_options', array()); 
+							$fullPrefixe = $options['RPJP_prefixe'] != '' ? $options['RPJP_prefixe'] . '-' : '';
+							$fullSuffixe = $options['RPJP_suffixe'] != '' ? '-' . $options['RPJP_suffixe'] : '';
+							update_post_meta( $newPostId, 'ref', sanitize_text_field( strtoupper($fullPrefixe) . get_the_date('mY', $newPostId) . "-" . $newPostId . strtoupper($fullSuffixe) ) );
+							//Le post est complet donc on peut le passer en publish
+							$arg = array(
+									'ID' => $newPostId,
+									'post_status' => 'publish',
+								);
+							wp_update_post( $arg ); //on passe le post en publié
+						}
+					}
 				}
 			}
 		}
